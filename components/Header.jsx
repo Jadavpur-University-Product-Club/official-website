0f2cd37cba736278ee4a6e011eca50b624266c65
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-05-31 23:40:18 +0530

    update globe

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,12 +16,12 @@ export default function Header() {
                     autoplay={true}
                     loop={true}
                     controls={false}
-                    src="https://assets8.lottiefiles.com/packages/lf20_YN73eT.json"
+                    src="https://assets5.lottiefiles.com/private_files/lf30_0gxfdakl.json"
                     style={{ height: '100%', width: '100%' }}
                 ></Player> 
             </div>
             {/* Background overlay */}
-            <div className="absolute bg-black opacity-70 inset-0 -z-10"></div>
+            <div className="absolute bg-black opacity-30 inset-0 -z-10"></div>
             {/* Text */}
             <div className="absolute z-0">
                 <p className="font-bruno-ace-sc text-4xl md:text-9xl text-header-color-1 text-center">LET&apos;S <span className="text-header-color-2">B</span>UILD</p>
